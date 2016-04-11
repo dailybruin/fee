97b6ad3ac2b5d3d737457591ac25b3e7ea24985f
thetoothless <ninaling@ucla.edu>
2016-04-11 11:53:19 -0700

    cleaned up some code

--- js/angular.js
+++ js/angular.js
@@ -1,6 +1,39 @@
 var myApp=angular.module("myApp", []);
 
-myApp.controller("myController", function(){});
+myApp.controller("myController", function(){
+    
+    $scope.navOptions=[
+        {
+            name: "Home",
+            link: "index.html"
+        },
+        {
+            name: "About",
+            link: "about.html"
+        },
+        {
+            name: "FAQ",
+            link: "faq.html"
+        },
+        {
+            name: "Finances",
+            link: "finances.html"
+        },
+        {
+            name: "Contact",
+            link: "contact.html"
+        },
+        {
+            name: "Vote",
+            link: "vote.html"
+        },
+    ]
+    
+    $scope.getOptions=function(){
+        return this.navOptions;
+    }
+
+});
 
 myApp.directive('ngNavBar', function() {
   return {
